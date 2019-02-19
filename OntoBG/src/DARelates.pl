f0fb0b47636e28cceac31c0ecaa891b9305041c7
Joshua Kritz <joshuakritz2@gmail.com>
2019-02-19 19:22:53 -0300

    adição das relações MD e AD. mais regras

---
+++ OntoBG/src/DARelates.pl
@@ -0,0 +1,13 @@
+daRelation(dynamics(deduction),emotion(pride)).
+daRelation(dynamics(deduction),emotion(ecstasy)).
+daRelation(dynamics(deduction),emotion(relief)).
+daRelation(dynamics(deduction),emotion(joy)).
+daRelation(dynamics(deduction),emotion(amusement)).
+daRelation(dynamics(deduction),emotion(excitement)).
+
+daRelation(dynamics(deduction),emotion(disapointment)).
+daRelation(dynamics(deduction),emotion(discouragement)).
+daRelation(dynamics(deduction),emotion(excitement)).
+
+
+
--- OntoBG/src/rules.pl
+++ OntoBG/src/rules.pl
@@ -1,2 +1,11 @@
 generalization(call(A,A1),call(B,B1)):- A\=B,A1\=B1.
 
+generalization(A,B):- generalization(A,C),generalization(C,B).
+
+mdRelation(A,B):-generalization(C,A),mdRelation(C,B).
+mdRelation(A,B):-generalization(C,B),mdRelation(A,C).
+
+daRelation(A,B):-generalization(C,A),daRelation(C,B).
+daRelation(A,B):-generalization(C,B),daRelation(A,C).
+
+daRelation(A,B):-partof(B,C), daRelation(C,A).
